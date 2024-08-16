1ad944274546d234966464211237fbfcc592897e
Mr_Wick <blackpanther.bp007@gmail.com>
2024-08-16 20:32:14 +0200

    added animated-counter and doughnut chart

---
+++ components/Sidebar.tsx
@@ -0,0 +1,45 @@
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const Sidebar = ({user}: SiderbarProps) => {
+
+    const pathname=usePathname();
+    
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" passHref>
+          <div className="mb-12 cursor-pointer items-center gap-2">
+            <Image
+              src="/icons/logo.svg"
+              width={36}
+              height={36}
+              alt="Horizon Logo"
+              className="size=[24px] max-xl:size-14"
+            />
+          <h1 className='sidebar-logo'>
+            Horizon</h1>
+          </div>
+        </Link>
+
+        {sidebarLinks.map((item)=> {
+
+            const isActive= pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+            return (
+            <Link href={item.route} key={item.label}
+                        className={cn('sidebar-link',{'bg-bank-gradient': isActive})}>
+                {item.label}
+            </Link>
+        )
+    })}
+
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
